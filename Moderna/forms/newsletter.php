cfb1e9a5b6469e29d7d1b290dfd63c15fc2bcf5f
applepie6000 <erickmutura05@gmail.com>
2025-05-06 17:16:33 +0300

    seven commit

--- Moderna/forms/newsletter.php
+++ Moderna/forms/newsletter.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'erickmutura05@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -17,14 +17,22 @@
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
-  
+
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['email'];
   $contact->from_email = $_POST['email'];
   $contact->subject ="New Subscription: " . $_POST['email'];
 
+    $contact->smtp = array(
+    'host' => 'erickmutura05@gmail.com',
+    'username' => 'erick',
+    'password' => 'bhvn xopq dmmv erad',
+    'port' => '465'
+  );
+
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
+
   $contact->smtp = array(
     'host' => 'example.com',
     'username' => 'example',
